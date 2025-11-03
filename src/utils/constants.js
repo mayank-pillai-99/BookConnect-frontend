1052431bd650c741d0f61427b47fdbc205afedf7
Mayank Pillai <mayankpillai99@gmail.com>
2025-11-03 18:51:11 +0530

    fix(socket): - Modify socket connection logic in createSocketConnection -BASE_URL usage in socket connection setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL= location.hostname==="localhost" ? "http://localhost:7777" : "https://bookconnect-backend.onrender.com" ;
+export const BASE_URL = location.hostname === "localhost" 
+  ? "http://localhost:7777" 
+  : "https://bookconnect-backend.onrender.com";
+
